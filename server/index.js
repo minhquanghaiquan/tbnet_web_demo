e6edf69ab4d21dc36825aa5640a1e79e2c14cc0b
minhquang <minhquanghaiquan@gmail.com>
2023-07-21 07:26:39 +0700

    use postgree database

--- server/index.js
+++ server/index.js
@@ -19,11 +19,21 @@ const jwt = require("jsonwebtoken");
 const shortId = require("shortid");
 
 //require db
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 // const mongodb = require("mongodb");
-const stations = require("./models/stationsModel");
-const listStations = require("./models/listStationsModel");
-const users = require("./models/userModel");
+// const stations = require("./models/stationsModel");
+// const listStations = require("./models/listStationsModel");
+// const users = require("./models/userModel");
+
+//require postgresql
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "tbnet_smartsite",
+  password: "123456",
+  port: 5432,
+});
 
 //require service mail
 const { OAuth2Client } = require("google-auth-library");
@@ -55,29 +65,91 @@ const key_token = process.env.KEY_TOKEN;
 
 //mongodb - port server
 const port_server = process.env.PORT_SERVER;
-const host_mongodb = process.env.HOST_MONGODB;
+// const host_mongodb = process.env.HOST_MONGODB;
+
 //mqtt broker
 const topic_mqtt = process.env.TOPIC_MQTT;
 const username_mqtt = process.env.USERNAME_MQTT;
 const password_mqtt = process.env.PASSWORD_MQTT;
+const clientId_mqtt = process.env.CLIENT_ID_MQTT;
 const client = mqtt.connect(process.env.HOST_MQTT, {
+  clientId: clientId_mqtt,
   username: username_mqtt,
   password: password_mqtt,
+  clean: true,
 });
 
 //Create server
 server.listen(port_server, async () => {
   console.log(`Example server listening on port ${port_server}`);
-  await mongoose.connect(host_mongodb);
+  // await mongoose.connect(host_mongodb);
+  const users = `
+    CREATE TABLE IF NOT EXISTS "public"."users" (
+      "id" BIGSERIAL ,
+        "login_user" VARCHAR(128) NOT NULL, 
+        "login_pwd" VARCHAR(1024) UNIQUE NOT NULL, 
+        "reset_token" VARCHAR(1024) UNIQUE, 
+        "expire_token" BIGINT UNIQUE, 
+        "created_at" TIMESTAMP,
+        PRIMARY KEY ("id")
+    );`;
+
+  const device = `
+    CREATE TABLE IF NOT EXISTS "public"."device" (
+        "id" BIGSERIAL,
+        "device_id" VARCHAR(50) NOT NULL, 
+        "model" VARCHAR(128) NOT NULL, 
+        "status" INTEGER NOT NULL, 
+        "version_fw" VARCHAR(1024) UNIQUE, 
+        "version_hw" VARCHAR(1024) UNIQUE, 
+        "expired_date" TIMESTAMP,
+        "created_at" TIMESTAMP,
+        PRIMARY KEY ("id")
+    );`;
+  const device_data = `
+    CREATE TABLE IF NOT EXISTS "public"."device_data" (
+      "id" BIGSERIAL,
+        "device_id" VARCHAR(50) NOT NULL, 
+        "s0" INTEGER NOT NULL,
+        "s1" INTEGER NOT NULL,
+        "created_at" TIMESTAMP,
+        PRIMARY KEY ("id")
+    );`;
+
+  await pool.query(users);
+  await pool.query(device);
+  await pool.query(device_data);
+
+  let SQL1 = "SELECT * FROM users WHERE login_user=$1;";
+  const check_user = await pool.query(SQL1, ["minhquangbvmi@gmail.com"]);
+  if (check_user.rows.length == 0) {
+    var SQL2 = "INSERT INTO users (login_user , login_pwd ) VALUES ($1 , $2 );";
+    await pool.query(SQL2, ["minhquangbvmi@gmail.com", "123456789"]);
+  }
+
+  let SQL3 = "SELECT * FROM device WHERE device_id=$1;";
+  const check_device1 = await pool.query(SQL3, ["A132323234554"]);
+  const check_device2 = await pool.query(SQL3, ["A132323234555"]);
+  console.log(check_device2.rows);
+  if (check_device1.rows.length == 0) {
+    var SQL4 =
+      "INSERT INTO device (device_id , model, status ) VALUES  ($1 , $2, $3 );";
+    await pool.query(SQL4, ["A132323234554", "X004", 1]);
+  }
+  if (check_device2.rows.length == 0) {
+    var SQL5 =
+      "INSERT INTO device (device_id , model, status ) VALUES ($1 , $2 , $3);";
+    await pool.query(SQL5, ["A132323234555", "X005", 1]);
+  }
 });
 
 //Connect to MongoDB
-mongoose.connection.on("connected", async () => {
-  console.log("MongoDB connected");
-});
-mongoose.connection.on("error", async () => {
-  console.log("MongoDB connect fail");
-});
+// mongoose.connection.on("connected", async () => {
+//   console.log("MongoDB connected");
+// });
+// mongoose.connection.on("error", async () => {
+//   console.log("MongoDB connect fail");
+// });
 
 //Connect to MQTT broker
 client.on("connect", async () => {
@@ -89,28 +161,29 @@ client.on("message", async (topic_mqtt, message) => {
   var data = message.toString();
   if (data) {
     data = JSON.parse(data);
-    data.created = new Date().toTimeString();
-    data._id = shortId.generate();
-    await stations.findOneAndUpdate(
-      { station_id: data.station_id },
-      { $push: { station_values: data } }
-    );
-    topic_mqtt = topic_mqtt + data.station_id;
-    sendDataToClient(topic_mqtt, data);
+    // data.created = new Date().toTimeString();
+    // data._id = shortId.generate();
+    console.log(new Date());
+    let SQL =
+      "INSERT INTO device_data (device_id , s0 , s1, created_at ) VALUES ( $1 , $2 , $3, $4 );";
+    await pool.query(SQL, [
+      data.header.serial_number,
+      data.payload.s0,
+      data.payload.s1,
+      new Date(),
+    ]);
+
+    var topic_mqtt_device = data.header.serial_number + "/control";
+    sendDataToClient(topic_mqtt_device, data);
   }
 });
 
 // Connect to client by socket io
 io.on("connection", (clientWeb) => {
-  console.log("Client connected" + clientWeb.id);
   clientWeb.on("CurrentControl", async (data) => {
-    console.log(data);
-    await stations.findOneAndUpdate(
-      { station_id: data.station_id },
-      { status_currentcontrol: data.status_currentcontrol },
-      { new: true }
-    );
-    sendToDevice("CurrentControl" + data.station_id, data);
+    let SQL = "UPDATE device SET status=$1 WHERE device_id=$2";
+    await pool.query(SQL, [data.status_currentcontrol, data.device_id]);
+    sendToDevice(data.device_id + "/control", data);
   });
 });
 
@@ -137,9 +210,10 @@ tokenIsValid = async (req, res, next) => {
     const verified = jwt.verify(token, key_token);
     if (!verified) return res.json(false);
 
-    const user = await users.findOne({ username: verified.username });
-    console.log(user);
-    if (!user) return res.json(false);
+    let SQL = "SELECT * FROM users WHERE login_user=$1;";
+    const user = await pool.query(SQL, [verified.login_user]);
+
+    if (user.rows.length == 0) return res.json(false);
     return next();
   } catch (err) {
     console.log(err);
@@ -153,33 +227,47 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/stations", tokenIsValid, async (req, res) => {
-  var getListStations = await listStations.find({});
-  console.log(getListStations);
-  return res.status(200).json(getListStations);
+  let SQL = "SELECT * FROM device;";
+  const user = await pool.query(SQL);
+  return res.status(200).json(user.rows);
 });
 
-app.get("/stations/:station_id", tokenIsValid, async (req, res) => {
-  var station_id = req.params.station_id;
-  var thisStation = await stations.findOne({ station_id: station_id });
-  var lastValue =
-    thisStation.station_values[thisStation.station_values.length - 1];
-  var status_currentcontrol = thisStation.status_currentcontrol;
+app.get("/stations/:device_id", tokenIsValid, async (req, res) => {
+  var device_id = req.params.device_id;
+  // let SQL = "SELECT * FROM device_data WHERE device_id=$1;";
+  // const device_data = await pool.query(SQL, [device_id]);
+  // var lastValue = device_data.rows[device_data.rows.length - 1];
+
+  let SQL1 =
+    "SELECT * FROM public.device_data WHERE device_id=$1 ORDER BY created_at DESC;";
+  const device_data = await pool.query(SQL1, [device_id]);
+  var lastValue = device_data.rows[0];
+
+  let SQL2 = "SELECT * FROM device WHERE device_id=$1;";
+  const device = await pool.query(SQL2, [device_id]);
+
+  var status_currentcontrol = device.rows[0].status;
   return res.status(200).json({ lastValue, status_currentcontrol });
 });
 
 app.post("/login", async (req, res) => {
   var { username, password } = req.body;
-  const user = await users.findOne({ username });
-  if (!user) return res.status(400).send("Username or password is wrong");
+  let SQL = "SELECT * FROM users WHERE login_user=$1;";
+  const user = await pool.query(SQL, [username]);
+  if (user.rows.length === 0)
+    return res.status(400).send("Username or password is wrong");
   //ss
-  bcrypt.compare(password, user.password).then((doMatch) => {
+  bcrypt.compare(password, user.rows[0].login_pwd).then((doMatch) => {
     if (doMatch) {
-      const token = jwt.sign({ username: user.username }, key_token);
+      const token = jwt.sign(
+        { login_user: user.rows[0].login_user },
+        key_token
+      );
       res.json({
         token,
         user: {
-          id: user._id,
-          email: user.username,
+          id: user.rows[0].login_user_id,
+          email: user.rows[0].login_user,
         },
       });
     } else {
@@ -189,7 +277,7 @@ app.post("/login", async (req, res) => {
 });
 
 app.post("/auth/forgotpassword", (req, res) => {
-  crypto.randomBytes(32, (err, buffer) => {
+  crypto.randomBytes(32, async (err, buffer) => {
     if (err) {
       console.log(err);
     }
@@ -197,82 +285,39 @@ app.post("/auth/forgotpassword", (req, res) => {
     console.log(req.body.emailUser);
     console.log(token);
 
-    users
-      .findOneAndUpdate(
-        { username: req.body.emailUser },
-        { $set: { resetToken: token, expireToken: Date.now() + 3600000 } }
-      )
-      .then(async (user) => {
-        const myAccessTokenObject = await myOAuth2Client.getAccessToken();
-        const myAccessToken = myAccessTokenObject?.token;
+    let SQL_select = "SELECT * FROM users WHERE login_user=$1;";
+    const user = await pool.query(SQL_select, [req.body.emailUser]);
 
-        const transport = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            type: "OAuth2",
-            user: ADMIN_EMAIL_ADDRESS,
-            clientId: GOOGLE_MAILER_CLIENT_ID,
-            clientSecret: GOOGLE_MAILER_CLIENT_SECRET,
-            refresh_token: GOOGLE_MAILER_REFRESH_TOKEN,
-            accessToken: myAccessToken,
-          },
-        });
+    let SQL = `UPDATE "users" 
+                SET "reset_token" = $1 ,"expire_token" = $2
+                WHERE "login_user" = $3`;
+    await pool.query(SQL, [token, Date.now() + 3600000, req.body.emailUser]);
 
-        const mailOptions = {
-          to: user.username, // Gửi đến ai?
-          subject: "Reset password - TBNET-WEB", // Tiêu đề email
-          html: `
+    const myAccessTokenObject = await myOAuth2Client.getAccessToken();
+    const myAccessToken = myAccessTokenObject?.token;
+
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: ADMIN_EMAIL_ADDRESS,
+        clientId: GOOGLE_MAILER_CLIENT_ID,
+        clientSecret: GOOGLE_MAILER_CLIENT_SECRET,
+        refresh_token: GOOGLE_MAILER_REFRESH_TOKEN,
+        accessToken: myAccessToken,
+      },
+    });
+
+    const mailOptions = {
+      to: user.rows[0].login_user, // Gửi đến ai?
+      subject: "Reset password - TBNET-WEB", // Tiêu đề email
+      html: `
           <p>You requested for password reset</p>
-          <h3>Click in this <a href="http://178.128.113.184:8081/reset/${token}">link</a> to reset password</h3>
+          <h3>Click in this <a href="http://localhost:8080/reset/${token}">link</a> to reset password</h3>
           `,
-        };
-        await transport.sendMail(mailOptions);
-        res.json({ message: "check your email" });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    // users.findOne({ username: req.body.emailUser }).then((user) => {
-    //   console.log(user);
-    //   if (!user) {
-    //     return res
-    //       .status(422)
-    //       .json({ error: "User dont exists with that email" });
-    //   }
-    //   user.resetToken = token;
-    //   user.expireToken = Date.now() + 3600000;
-    //   console.log(user);
-    //   user.save().then(async (result) => {
-    //     const myAccessTokenObject = await myOAuth2Client.getAccessToken();
-    //     const myAccessToken = myAccessTokenObject?.token;
-
-    //     const transport = nodemailer.createTransport({
-    //       service: "gmail",
-    //       auth: {
-    //         type: "OAuth2",
-    //         user: ADMIN_EMAIL_ADDRESS,
-    //         clientId: GOOGLE_MAILER_CLIENT_ID,
-    //         clientSecret: GOOGLE_MAILER_CLIENT_SECRET,
-    //         refresh_token: GOOGLE_MAILER_REFRESH_TOKEN,
-    //         accessToken: myAccessToken,
-    //       },
-    //     });
-
-    //     const mailOptions = {
-    //       to: user.username, // Gửi đến ai?
-    //       subject: "Reset password - TBNET-WEB", // Tiêu đề email
-    //       html: `
-    //       <p>You requested for password reset</p>
-    //       <h3>Click in this <a href="http://localhost:8080/reset/${token}">link</a> to reset password</h3>
-    //       `,
-    //     };
-
-    //     await transport.sendMail(mailOptions);
-
-    //     res.json({ message: "check your email" });
-    //   });
-    // });
+    };
+    await transport.sendMail(mailOptions);
+    res.json({ message: "check your email" });
   });
 });
 
@@ -286,23 +331,18 @@ app.post("/new-password", async (req, res) => {
   const newPassword = req.body.password;
   const sentToken = req.body.token;
   const hashedpassword = await bcrypt.hash(newPassword, 12);
-  users
-    .findOneAndUpdate(
-      { resetToken: sentToken, expireToken: { $gt: Date.now() } },
-      {
-        $set: {
-          password: hashedpassword,
-          expireToken: null,
-          resetToken: null,
-        },
-      },
-      { new: true }
-    )
-    .then((saveduser) => {
-      console.log(saveduser);
-      res.json({ message: "password updated success" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+
+  let SQL_user =
+    "SELECT * FROM users WHERE reset_token=$1 AND expire_token>$2;";
+  const user = await pool.query(SQL_user, [sentToken, Date.now()]);
+  console.log(user.rows);
+
+  let SQL =
+    "UPDATE users SET login_pwd=$1 ,reset_token=$2 ,expire_token=$3 WHERE reset_token=$4 AND expire_token>$5";
+
+  await pool.query(SQL, [hashedpassword, "", 0, sentToken, Date.now()]);
+
+  if (user.rows.length !== 0) {
+    res.json({ message: "password updated success" });
+  }
 });
