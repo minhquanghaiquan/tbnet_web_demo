46c477b7268d6975c46bb9274797246dba139a2f
minhquang <minhquanghaiquan@gmail.com>
2024-01-06 22:35:41 +0700

    update 6/1

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const path = require("path");
-const sql = require('sql');
+const sql = require("sql");
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
@@ -30,7 +30,7 @@ const shortId = require("shortid");
 const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
+  host: "178.128.113.184",
   database: "tbnet_smartsite",
   password: "123456",
   port: 5432,
@@ -83,30 +83,51 @@ const client = mqtt.connect(process.env.HOST_MQTT, {
 //Create server
 server.listen(port_server, async () => {
   console.log(`Example server listening on port ${port_server}`);
-  await pool.query(process.env.CREATE_USERS);
+  // const result = await pool.query(process.env.CREATE_USERS);
   await pool.query(process.env.CREATE_DEVICE);
   await pool.query(process.env.CREATE_DEVICE_DATA);
-
+  await pool.query(process.env.CREATE_USERS);
   let SQL1 = "SELECT * FROM users WHERE login_user=$1;";
   const check_user = await pool.query(SQL1, ["minhquangbvmi@gmail.com"]);
+  // console.log(check_user);
   if (check_user.rows.length == 0) {
     var SQL2 = "INSERT INTO users (login_user , login_pwd ) VALUES ($1 , $2 );";
     await pool.query(SQL2, ["minhquangbvmi@gmail.com", "123456789"]);
   }
 
   let SQL3 = "SELECT * FROM device WHERE device_id=$1;";
-  const check_device1 = await pool.query(SQL3, ["A132323234554"]);
-  const check_device2 = await pool.query(SQL3, ["A132323234555"]);
+  const check_device1 = await pool.query(SQL3, ["HTP001"]);
+  const check_device2 = await pool.query(SQL3, ["HTP002"]);
+  const check_device3 = await pool.query(SQL3, ["CTOM28"]);
+  const check_device4 = await pool.query(SQL3, ["CTNK74"]);
 
   if (check_device1.rows.length == 0) {
     var SQL4 =
-      "INSERT INTO device (device_id , model, status ) VALUES  ($1 , $2, $3 );";
-    await pool.query(SQL4, ["A132323234554", "X004", 1]);
+      "INSERT INTO device (device_id , address, status ) VALUES  ($1 , $2, $3 );";
+    await pool.query(SQL4, [
+      "HTP001",
+      "Hồ Chí Minh, Tân Phú, 212 Lê Trọng Tấn",
+      1,
+    ]);
   }
   if (check_device2.rows.length == 0) {
     var SQL5 =
-      "INSERT INTO device (device_id , model, status ) VALUES ($1 , $2 , $3);";
-    await pool.query(SQL5, ["A132323234555", "X005", 1]);
+      "INSERT INTO device (device_id , address, status ) VALUES ($1 , $2 , $3);";
+    await pool.query(SQL5, [
+      "HTP002",
+      "Hồ Chí Minh, Tân Phú, 2 Bờ Bao Tân Thắng",
+      1,
+    ]);
+  }
+  if (check_device3.rows.length == 0) {
+    var SQL6 =
+      "INSERT INTO device (device_id , address, status ) VALUES ($1 , $2 , $3);";
+    await pool.query(SQL6, ["CTOM28", "Cần Thơ, Ô Môn", 1]);
+  }
+  if (check_device4.rows.length == 0) {
+    var SQL7 =
+      "INSERT INTO device (device_id , address, status ) VALUES ($1 , $2 , $3);";
+    await pool.query(SQL7, ["CTNK74", "Cần Thơ, Ninh Kiều", 1]);
   }
 });
 
@@ -118,39 +139,52 @@ client.on("connect", async () => {
 
 client.on("message", async (topic_mqtt, message) => {
   var data = message.toString();
-  if (data) {
-    data = JSON.parse(data);
+  data = JSON.parse(data);
+  console.log(data);
+  try {
+    if (data.header.version) {
+      const ColumsDeviceData = () => {
+        let columnsInsert = ["id", "device_id"];
+        for (var i = 0; i < 997; i++) {
+          columnsInsert.push("s" + i);
+        }
+        columnsInsert.push("created_at");
+        return columnsInsert;
+      };
 
-    const ColumsDeviceData = () => {
-      let columnsInsert = ["id", "device_id"];
-      for (var i = 0; i < 121; i++) {
-        columnsInsert.push("s" + i);
+      let device_data = sql.define({
+        name: "device_data",
+        columns: ColumsDeviceData(),
+      });
+
+      let data_insert = {};
+      for (var i = 0; i < 996; i++) {
+        data_insert["s" + i] = data.payload["s" + i];
       }
-      columnsInsert.push("created_at");
-      return columnsInsert;
-    };
+      console.log(data_insert);
+      data_insert.device_id = data.header.serial_number;
+      let query = device_data.insert([data_insert]).returning("*").toQuery();
+      await pool.query(query);
 
-    let device_data = sql.define({
-      name: "device_data",
-      columns: ColumsDeviceData(),
-    });
+      var topic_mqtt_device = data.header.serial_number + "/control";
 
-    let data_insert = data.payload;
-    data_insert.device_id = data.header.serial_number;
-    let query = device_data.insert([data_insert]).returning("*").toQuery();
-    await pool.query(query);
-
-    var topic_mqtt_device = data.header.serial_number + "/control";
-    sendDataToClient(topic_mqtt_device, data);
+      sendDataToClient(topic_mqtt_device, data);
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
-// Connect to client by socket io
+// Connect to client by socket io ORDER BY created_at DESC
 io.on("connection", (clientWeb) => {
   clientWeb.on("CurrentControl", async (data) => {
-    let SQL = "UPDATE device SET status=$1 WHERE device_id=$2";
-    await pool.query(SQL, [data.status_currentcontrol, data.device_id]);
-    sendToDevice(data.device_id + "/control", data);
+    console.log(data);
+    // let SQL =
+    //   "UPDATE device_data SET " +
+    //   data.key +
+    //   "= $1 WHERE device_id = $2 AND id = (SELECT MAX(id) FROM device_data WHERE device_id = $3);";
+    // await pool.query(SQL, [data.value, data.device_id, data.device_id]);
+    // sendToDevice(data.device_id + "/control", data);
   });
 });
 
@@ -161,6 +195,7 @@ sendDataToClient = async (topic, msg) => {
 
 sendToDevice = async (topic, message) => {
   let data = JSON.stringify(message);
+  console.log(data);
   await client.publish(topic, data);
 };
 
@@ -207,11 +242,11 @@ app.get("/stations/:device_id", tokenIsValid, async (req, res) => {
   const device_data = await pool.query(SQL1, [device_id]);
   var lastValue = device_data.rows[0];
 
-  let SQL2 = "SELECT * FROM device WHERE device_id=$1;";
-  const device = await pool.query(SQL2, [device_id]);
+  // let SQL2 = "SELECT * FROM device WHERE device_id=$1;";
+  // const device = await pool.query(SQL2, [device_id]);
 
-  var status_currentcontrol = device.rows[0].status;
-  return res.status(200).json({ lastValue, status_currentcontrol });
+  // var status_currentcontrol = device.rows[0].status;
+  return res.status(200).json({ lastValue });
 });
 
 app.post("/login", async (req, res) => {
